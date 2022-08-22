fdd1e7f9197fb70ec38b67ff80030a625c7efbc1
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-08-22 17:47:29 -0500

    Created the outline for the questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Author
+    "Who is the author of the README?",
+    // About this Project
+    "What does your application do?",
+    "What was your motivation behind the project?",
+    "Why did you use the technologies you used?",
+    "What're some challenges you faced during creation?",
+    "What're your goals for future implications?",
+    // Table of Contents 
+    ["Would you like a table of contents?"],
+    // How to install
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +24,42 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+const howToInstall = [
+    "1. Navigate to the code repository",
+    "2. Press the green code button, located near the about section",
+    "3. Copy either the HTTPS, Git CLI, download the zip, open with GitHub desktop, or copy the SSH link.",
+    "4. Depending on download method, use Git, executable, or the desktop application to open the content files",
+    "5. All of the content of the repository will be available after completion of the previous state."
+];
+
+console.log(howToInstall);
+
+// Author 
+
+// question: who is the author of the readme?
+
+
+// About this project 
+// what does your application do?
+// Why did you use the technologies you used?
+// some challeneges faced during creation 
+// goals for future implications 
+
+// table of contents (optional) 
+
+// how to install and run the project 
+// copy from other readme 
+
+// how to use this project 
+// provide steps for the proejct 
+// ask how many images or videos they would like to include 
+// ask for alt text 
+// ask for image href 
+
+// include credits 
+// ask if they worked with anyone else 
+// links to github and social media 
+
+// add a license 
+// give them a license choice 
